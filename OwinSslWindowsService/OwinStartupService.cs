8b4b437222c12783aea2469ff2258a4f3abd17c2
yadavvineet <vineetyadavonline@gmail.com>
2015-11-22 03:45:49 +0530

    init for OwinSslWindowsService

---
+++ OwinSslWindowsService/OwinStartupService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace OwinSslWindowsService
+{
+    public partial class OwinStartupService : ServiceBase
+    {
+        public OwinStartupService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
